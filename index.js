2102f9f3aaa2223b02572d32eaf9d1848021676f
Julius Tens <mail@juliustens.eu>
2018-08-29 15:27:17 +0200

    add packageExports, packageJson methods

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 'use strict'
 
+const packageJson = require('./lib/packageJson')
+const packageExports = require('./lib/packageExports')
+
+module.exports = {packageJson, packageExports}
--- test.js
+++ test.js
@@ -1,8 +1,68 @@
 'use strict'
 
 const tape = require('tape')
-const boilerplate = require('./index')
+const tests = require('./index')
 
-tape('boilerplate', (t) => {
-	
+tape('fptf-tests.packageJson', async (t) => {
+	const validPackage1 = {
+		dependencies: {
+			'validate-fptf': '2.1.1'
+		}
+	}
+	const validPackage2 = {
+		devDependencies: {
+			'validate-fptf': '2.1.1'
+		}
+	}
+	const invalidPackage1 = {}
+	const invalidPackage2 = {dependencies: {
+		'validate-fptf': '0.0.0'
+	}}
+
+	await tests.packageJson(validPackage1).then(() => t.pass()).catch(() => t.fail())
+	await tests.packageJson(validPackage1).then(() => t.pass()).catch(() => t.fail())
+	await tests.packageJson(invalidPackage1).then(() => t.fail()).catch(() => t.pass())
+	await tests.packageJson(invalidPackage2).then(() => t.fail()).catch(() => t.pass())
+	t.end()
+})
+
+tape('fptf-tests.packageExports', (t) => {
+	const package1 = {
+		journeys: () => 1,
+		stations: {
+			all: () => 1,
+			nearby: () => 1
+		}
+	}
+	const package2 = {
+		journeys: () => 1,
+		stops: {
+			nearby: () => 1
+		},
+		regions: {
+			all: () => 1
+		},
+		otherMethod: () => 1
+	}
+	const package3 = {
+		journeys: () => 1,
+		stopovers: 1
+	}
+	const package4 = {
+		otherMethod: 1
+	}
+
+	t.ok(tests.packageExports(package1, ['journeys', 'stations.all', 'stations.nearby']))
+	t.throws(() => tests.packageExports(package1, ['journeys', 'stations.nearby']))
+	t.throws(() => tests.packageExports(package1, ['journeys', 'stations.nearby', 'stations.all', 'stopovers']))
+
+	t.ok(tests.packageExports(package2, ['journeys', 'regions.all', 'stops.nearby']))
+
+	t.throws(() => tests.packageExports(package3, ['journeys', 'stopovers']))
+	t.throws(() => tests.packageExports(package3, ['journeys']))
+
+	t.throws(() => tests.packageExports(package4, ['stations.all']))
+	t.throws(() => tests.packageExports(package4, []))
+
+	t.end()
 })
